9fb3293cba85cf9e00f6df103e25220abbc944c5
Simon Willison <swillison@gmail.com>
2023-06-15 17:22:13 +0100

    Now uses API key from keys.json, refs #13

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -33,3 +33,47 @@ def test_keys_set(monkeypatch, tmpdir):
         "// Note": "This file stores secret API credentials. Do not share!",
         "openai": "foo",
     }
+
+
+def test_uses_correct_key(requests_mock, monkeypatch, tmpdir):
+    keys_path = tmpdir / "keys.json"
+    keys_path.write_text(
+        json.dumps(
+            {
+                "openai": "from-keys-file",
+                "other": "other-key",
+            }
+        ),
+        "utf-8",
+    )
+    monkeypatch.setenv("LLM_KEYS_PATH", str(keys_path))
+    monkeypatch.setenv("OPENAI_API_KEY", "from-env")
+    mocked = requests_mock.post(
+        "https://api.openai.com/v1/chat/completions",
+        json={"choices": [{"message": {"content": "Bob, Alice, Eve"}}]},
+        headers={"Content-Type": "application/json"},
+    )
+
+    def assert_key(key):
+        assert mocked.last_request.headers["Authorization"] == "Bearer {}".format(key)
+
+    runner = CliRunner()
+    # Called without --key uses environment variable
+    result = runner.invoke(cli, ["hello"], catch_exceptions=False)
+    assert result.exit_code == 0
+    assert_key("from-env")
+    # Called without --key and with no environment variable uses keys.json
+    monkeypatch.setenv("OPENAI_API_KEY", "")
+    result2 = runner.invoke(cli, ["hello"], catch_exceptions=False)
+    assert result2.exit_code == 0
+    assert_key("from-keys-file")
+    # Called with --key name-in-keys.json uses that value
+    result3 = runner.invoke(cli, ["hello", "--key", "other"], catch_exceptions=False)
+    assert result3.exit_code == 0
+    assert_key("other-key")
+    # Called with --key something-else uses exactly that
+    result4 = runner.invoke(
+        cli, ["hello", "--key", "custom-key"], catch_exceptions=False
+    )
+    assert result4.exit_code == 0
+    assert_key("custom-key")
