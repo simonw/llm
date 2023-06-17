1346d4086ce4fe915bd96ce482a8f928a6768ad9
Simon Willison <swillison@gmail.com>
2023-06-17 09:55:02 +0100

    Copy button for docs, closes #43

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ from subprocess import PIPE, Popen
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx_copybutton"]
 myst_enable_extensions = ["colon_fence"]
 
 # Add any paths that contain templates here, relative to this directory.
