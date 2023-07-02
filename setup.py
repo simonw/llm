9eeebfa020894ab6591213390c1b19d4e8ff552f
Simon Willison <swillison@gmail.com>
2023-07-02 12:39:37 -0700

    type stubs for PyYAML and requests, refs #77

--- setup.py
+++ setup.py
@@ -41,6 +41,16 @@ setup(
         "PyYAML",
         "pluggy",
     ],
-    extras_require={"test": ["pytest", "requests-mock", "cogapp", "mypy", "black"]},
+    extras_require={
+        "test": [
+            "pytest",
+            "requests-mock",
+            "cogapp",
+            "mypy",
+            "black",
+            "types-PyYAML",
+            "types-requests",
+        ]
+    },
     python_requires=">=3.7",
 )
