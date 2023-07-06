04568115b81230638a27b4a2715b308dae12e077
Simon Willison <swillison@gmail.com>
2023-07-05 20:47:36 -0700

    Options base class is now llm.Options not Model.Options

--- llm/__init__.py
+++ llm/__init__.py
@@ -1,13 +1,14 @@
 from .hookspecs import hookimpl
-from .models import LogMessage, Model, Prompt, Response, OptionsError
+from .models import LogMessage, Model, Options, Prompt, Response, OptionsError
 from .templates import Template
 
 __all__ = [
-    "Template",
+    "hookimpl",
+    "LogMessage",
     "Model",
+    "Options",
+    "OptionsError",
     "Prompt",
     "Response",
-    "OptionsError",
-    "LogMessage",
-    "hookimpl",
+    "Template",
 ]
