72075547196eca2fd223873dc7f803b857b7e062
Simon Willison <swillison@gmail.com>
2023-08-11 23:18:46 -0700

    Updated test for new alignment of aliases, refs #151

--- tests/test_aliases.py
+++ tests/test_aliases.py
@@ -7,11 +7,11 @@ def test_aliases_list():
     result = runner.invoke(cli, ["aliases", "list"])
     assert result.exit_code == 0
     assert result.output == (
-        "3.5 = gpt-3.5-turbo\n"
-        "chatgpt = gpt-3.5-turbo\n"
-        "chatgpt-16k = gpt-3.5-turbo-16k\n"
-        "3.5-16k = gpt-3.5-turbo-16k\n"
-        "4 = gpt-4\n"
-        "gpt4 = gpt-4\n"
-        "4-32k = gpt-4-32k\n"
+        "3.5         : gpt-3.5-turbo\n"
+        "chatgpt     : gpt-3.5-turbo\n"
+        "chatgpt-16k : gpt-3.5-turbo-16k\n"
+        "3.5-16k     : gpt-3.5-turbo-16k\n"
+        "4           : gpt-4\n"
+        "gpt4        : gpt-4\n"
+        "4-32k       : gpt-4-32k\n"
     )
