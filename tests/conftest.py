b0ed8316e908b5bdb40a5be5d37ceffb919a7695
Simon Willison <swillison@gmail.com>
2023-08-27 17:29:35 -0700

    Initial tests with EmbedDemo embedding model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import llm
+from llm.plugins import pm
 
 
 def pytest_configure(config):
@@ -24,6 +26,33 @@ def templates_path(user_path):
 @pytest.fixture(autouse=True)
 def env_setup(monkeypatch, user_path):
     monkeypatch.setenv("LLM_USER_PATH", str(user_path))
+
+
+class EmbedDemo(llm.EmbeddingModel):
+    model_id = "embed-demo"
+
+    def embed(self, text):
+        words = text.split()[:16]
+        embedding = [len(word) for word in words]
+        # Pad with 0 up to 16 words
+        embedding += [0] * (16 - len(embedding))
+        return embedding
+
+
+@pytest.fixture(autouse=True)
+def register_embed_demo_model():
+    class EmbedDemoPlugin:
+        __name__ = "EmbedDemoPlugin"
+
+        @llm.hookimpl
+        def register_embedding_models(self, register):
+            register(EmbedDemo())
+
+    pm.register(EmbedDemoPlugin(), name="undo-embed-demo-plugin")
+    try:
+        yield
+    finally:
+        pm.unregister(name="undo-embed-demo-plugin")
 
 
 @pytest.fixture
