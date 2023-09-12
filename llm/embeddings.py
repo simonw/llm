267e2ea999992bbd019406cb32653b732e64a561
Simon Willison <swillison@gmail.com>
2023-09-11 22:44:22 -0700

    Reuse embeddings for hashed content, --store now works on second run - closes #224

--- llm/embeddings.py
+++ llm/embeddings.py
@@ -130,14 +130,20 @@ class Collection:
             metadata (dict, optional): Metadata to be stored
             store (bool, optional): Whether to store the value in the content or content_blob column
         """
-        from llm import encode
+        from llm import decode, encode
 
         content_hash = self.content_hash(value)
-        if self.db["embeddings"].count_where(
-            "content_hash = ? and collection_id = ?", [content_hash, self.id]
-        ):
-            return
-        embedding = self.model().embed(value)
+        existing = list(
+            self.db["embeddings"].rows_where(
+                "content_hash = ?", [content_hash], limit=1
+            )
+        )
+        if existing:
+            # Reuse the embedding from whatever record this is, it might even
+            # be in a different collection
+            embedding = decode(existing[0]["embedding"])
+        else:
+            embedding = self.model().embed(value)
         cast(Table, self.db["embeddings"]).insert(
             {
                 "collection_id": self.id,
@@ -192,23 +198,39 @@ class Collection:
             # Calculate hashes first
             items_and_hashes = [(item, self.content_hash(item[1])) for item in batch]
             # Any of those hashes already exist?
-            existing_ids = [
-                row["id"]
+            hashes_and_embeddings = {
+                row["content_hash"]: row["embedding"]
                 for row in self.db.query(
                     """
-                    select id from embeddings
-                    where collection_id = ? and content_hash in ({})
+                    select content_hash, embedding from embeddings
+                    where content_hash in ({})
                     """.format(
                         ",".join("?" for _ in items_and_hashes)
                     ),
-                    [collection_id]
-                    + [item_and_hash[1] for item_and_hash in items_and_hashes],
+                    [item_and_hash[1] for item_and_hash in items_and_hashes],
                 )
+            }
+            # We need to embed the ones that don't exist yet
+            items_to_embed = [
+                item_and_hash[0]
+                for item_and_hash in items_and_hashes
+                if item_and_hash[1] not in hashes_and_embeddings
             ]
-            filtered_batch = [item for item in batch if item[0] not in existing_ids]
-            embeddings = list(
-                self.model().embed_multi(item[1] for item in filtered_batch)
+            calculated_embeddings = list(
+                self.model().embed_multi(item[1] for item in items_to_embed)
             )
+
+            # This should be a list of all the embeddings from both sources
+            embeddings = []
+            calculated_embeddings_iterator = iter(calculated_embeddings)
+            for item_and_hash in items_and_hashes:
+                if item_and_hash[1] in hashes_and_embeddings:
+                    embeddings.append(
+                        llm.decode(hashes_and_embeddings[item_and_hash[1]])
+                    )
+                else:
+                    embeddings.append(next(calculated_embeddings_iterator))
+
             with self.db.conn:
                 cast(Table, self.db["embeddings"]).insert_all(
                     (
@@ -226,9 +248,7 @@ class Collection:
                             "metadata": json.dumps(metadata) if metadata else None,
                             "updated": int(time.time()),
                         }
-                        for (embedding, (id, value, metadata)) in zip(
-                            embeddings, filtered_batch
-                        )
+                        for (embedding, (id, value, metadata)) in zip(embeddings, batch)
                     ),
                     replace=True,
                 )
