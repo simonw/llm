deaa686d9cb115639e4160c40629cbf4d3878ec4
Simon Willison <swillison@gmail.com>
2023-11-06 12:03:07 -0800

    Pin to openai<1.0 - refs #327

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     """,
     install_requires=[
         "click",
-        "openai",
+        "openai<1.0",
         "click-default-group>=1.2.3",
         "sqlite-utils>=3.35.0",
         "sqlite-migrate>=0.1a2",
