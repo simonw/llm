3352eb9f577d4360529b29fbaef725536d5a8882
Simon Willison <swillison@gmail.com>
2024-11-06 03:27:25 -0800

    Serialize usage to JSON properly, closes #614

--- llm/default_plugins/openai_models.py
+++ llm/default_plugins/openai_models.py
@@ -514,7 +514,7 @@ def combine_chunks(chunks: List) -> dict:
 
     for item in chunks:
         if item.usage:
-            usage = dict(item.usage)
+            usage = item.usage.dict()
         for choice in item.choices:
             if choice.logprobs and hasattr(choice.logprobs, "top_logprobs"):
                 logprobs.append(
