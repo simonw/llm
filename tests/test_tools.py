a50de8b57a8673b6100c83ea313b4a9c554ac9b9
Simon Willison <swillison@gmail.com>
2025-05-13 09:41:46 -0700

    ChainResponse.log_to_db() method and test, refs #1017, #1003

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,8 @@
 import llm
+from llm.migrations import migrate
 import os
 import pytest
+import sqlite_utils
 
 
 API_KEY = os.environ.get("PYTEST_OPENAI_API_KEY", None) or "badkey"
@@ -28,3 +30,33 @@ def test_tool_use_basic(vcr):
     assert len(second.prompt.tool_results) == 1
     assert second.prompt.tool_results[0].name == "multiply"
     assert second.prompt.tool_results[0].output == "2869461"
+
+    # Test writing to the database
+    db = sqlite_utils.Database(memory=True)
+    migrate(db)
+    chain_response.log_to_db(db)
+    assert set(db.table_names()).issuperset(
+        {"tools", "tool_responses", "tool_calls", "tool_results"}
+    )
+
+    responses = list(db["responses"].rows)
+    assert len(responses) == 2
+    first_response, second_response = responses
+
+    tools = list(db["tools"].rows)
+    assert len(tools) == 1
+    assert tools[0]["name"] == "multiply"
+    assert tools[0]["description"] == "Multiply two numbers."
+
+    tool_results = list(db["tool_results"].rows)
+    tool_calls = list(db["tool_calls"].rows)
+
+    assert len(tool_calls) == 1
+    assert tool_calls[0]["response_id"] == first_response["id"]
+    assert tool_calls[0]["name"] == "multiply"
+    assert tool_calls[0]["arguments"] == '{"a": 1231, "b": 2331}'
+
+    assert len(tool_results) == 1
+    assert tool_results[0]["response_id"] == second_response["id"]
+    assert tool_results[0]["output"] == "2869461"
+    assert tool_results[0]["tool_call_id"] == tool_calls[0]["tool_call_id"]
