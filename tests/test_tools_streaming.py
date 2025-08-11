8d3ceec923aa1dad196b9fdcc07224c9f9b6f171
Simon Willison <swillison@gmail.com>
2025-08-11 14:00:24 -0700

    Ran black

--- tests/test_tools_streaming.py
+++ tests/test_tools_streaming.py
@@ -13,13 +13,17 @@ API_KEY = os.environ.get("PYTEST_OPENAI_API_KEY", None) or "badkey"
 def test_tools_streaming_variant_a():
     model = llm.get_model("gpt-4.1-mini")
 
-    chain = model.chain("What is the current llm version?", tools=[llm_version], key=API_KEY)
+    chain = model.chain(
+        "What is the current llm version?", tools=[llm_version], key=API_KEY
+    )
 
     output = "".join(chain)
 
     print(chain._responses)
 
-    assert "".join(str(output)) == "The current version of *llm* is **{}**.".format(version("llm"))
+    assert "".join(str(output)) == "The current version of *llm* is **{}**.".format(
+        version("llm")
+    )
 
 
 # This response contains streaming variant "b" where arguments="{}" is the first partial stream received.
@@ -27,13 +31,17 @@ def test_tools_streaming_variant_a():
 def test_tools_streaming_variant_b():
     model = llm.get_model("gpt-4.1-mini")
 
-    chain = model.chain("What is the current llm version?", tools=[llm_version], key=API_KEY)
+    chain = model.chain(
+        "What is the current llm version?", tools=[llm_version], key=API_KEY
+    )
 
     output = "".join(chain)
 
     print(chain._responses)
 
-    assert "".join(str(output)) == "The current version of *llm* is **{}**.".format(version("llm"))
+    assert "".join(str(output)) == "The current version of *llm* is **{}**.".format(
+        version("llm")
+    )
 
 
 # This response contains streaming variant "c".
@@ -41,10 +49,14 @@ def test_tools_streaming_variant_b():
 def test_tools_streaming_variant_c():
     model = llm.get_model("gpt-4.1-mini")
 
-    chain = model.chain("What is the current llm version?", tools=[llm_version], key=API_KEY)
+    chain = model.chain(
+        "What is the current llm version?", tools=[llm_version], key=API_KEY
+    )
 
     output = "".join(chain)
 
     print(chain._responses)
 
-    assert "".join(str(output)) == "The installed version of LLM on this system is {}.".format(version("llm"))
+    assert "".join(
+        str(output)
+    ) == "The installed version of LLM on this system is {}.".format(version("llm"))
