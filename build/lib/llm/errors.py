71169a38e92223d84a7f734f85262cc489229474
Darin Kishore <darinkishore@protonmail.com>
2025-08-25 12:22:00 -0700

    chore: remove build artifacts from repo

--- build/lib/llm/errors.py
+++
@@ -1,6 +0,0 @@
-class ModelError(Exception):
-    "Models can raise this error, which will be displayed to the user"
-
-
-class NeedsKeyException(ModelError):
-    "Model needs an API key which has not been provided"
